eb09d9dba8a78f054af5bf276c3d73a03c9e9275
mdel <mdelefm@gmail.com>
2022-12-08 00:44:42 -0800

    change url

--- components/header/header.js
+++ components/header/header.js
@@ -4,7 +4,7 @@ import './header.scss'
 function Header() {
 
 const handleSample = async() => {
-   const result = await fetch('/.netlify/functions/sample')
+   const result = await fetch('https://lustrous-crepe-401a2c.netlify.app/.netlify/functions/sample')
 }
 
   return (
