5e61d63ec87f69a6c964a81cbdc051f7cd691f6f
mdel <mdelefm@gmail.com>
2022-12-08 03:09:06 -0800

    revise mongodb connection

--- my_functions/sample.js
+++ my_functions/sample.js
@@ -1,13 +1,15 @@
 const { MongoClient } = require("mongodb");
 require('dotenv').config()
 
-const mongoClient = new MongoClient(process.env.MONGODB_URI);
+const mongoClient = new MongoClient('mongodb+srv://mdel_efm:Nov14151@mernshopping.jrbae.mongodb.net/resumeDB?retryWrites=true&w=majority');
+// const mongoClient = new MongoClient(process.env.MONGODB_URI);
 
-const clientPromise = mongoClient.connect();
+// const clientPromise = mongoClient.connect();
 
 const handler = async (event) => {
     try {
-       
+        const clientPromise =await mongoClient.connect();
+        console.log("Connected")
         const collection = (await clientPromise).collection(process.env.MONGODB_COLLECTION);
         // const database = (await clientPromise).db(process.env.MONGODB_DATABASE);
         // const collection = database.collection(process.env.MONGODB_COLLECTION);
