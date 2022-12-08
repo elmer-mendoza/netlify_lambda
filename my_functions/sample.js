1f33abdb4fd702a754b9ee7bae13b449d0912ee5
mdel <mdelefm@gmail.com>
2022-12-08 02:26:11 -0800

    connect to mongodb

--- my_functions/sample.js
+++ my_functions/sample.js
@@ -1,8 +1,22 @@
-exports.handler = async function (event, context) {
+const { MongoClient } = require("mongodb");
+require('dotenv').config()
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "elmer pogi" })
+const mongoClient = new MongoClient(process.env.MONGODB_URI);
+
+const clientPromise = mongoClient.connect();
+
+const handler = async (event) => {
+    try {
+        const database = (await clientPromise).db(process.env.MONGODB_DATABASE);
+        const collection = database.collection(process.env.MONGODB_COLLECTION);
+        const results = await collection.find({});
+        return {
+            statusCode: 200,
+            body: JSON.stringify(results),
+        }
+    } catch (error) {
+        return { statusCode: 500, body: error.toString() }
     }
-  
 }
+
+module.exports = { handler }
