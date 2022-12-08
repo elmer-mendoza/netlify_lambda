c8fa734285ce3971a5d7e908ac6db3988cf43aff
mdel <mdelefm@gmail.com>
2022-12-08 03:42:12 -0800

    change uri

--- my_functions/sample.js
+++ my_functions/sample.js
@@ -1,19 +1,25 @@
 const { MongoClient } = require("mongodb");
 require('dotenv').config()
+const mongoose = require('mongoose')
 
-const mongoClient = new MongoClient('mongodb+srv://mdel_efm:Nov14151@mernshopping.jrbae.mongodb.net/?retryWrites=true&w=majority');
-// const mongoClient = new MongoClient(process.env.MONGODB_URI);
+mongoose
+.connect(process.env.DATABASE_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
+.then(()=>console.log('MongoDB connected...'))
+.catch(err=>console.log(err));
 
-const clientPromise = mongoClient.connect();
+// const mongoClient = new MongoClient('mongodb+srv://mdel_efm:Nov14151@mernshopping.jrbae.mongodb.net/?retryWrites=true&w=majority');
+// // const mongoClient = new MongoClient(process.env.MONGODB_URI);
+
+// const clientPromise = mongoClient.connect();
 
 const handler = async (event) => {
     try {
        
-        
+       
         // const collection = (await clientPromise).collection(process.env.MONGODB_COLLECTION);
         // const database = (await clientPromise).db(process.env.MONGODB_DATABASE);
         // const collection = database.collection(process.env.MONGODB_COLLECTION);
-        const database = (await clientPromise).db('resumeDB');
+        const database =  mongoose.Connection.db('resumeDB');
         const collection = database.collection('resumeData');
         const results = await collection.find();
         return {
