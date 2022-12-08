284ba0c93b40c34bfb644dd085b548524efc305d
mdel <mdelefm@gmail.com>
2022-12-08 02:50:12 -0800

    change uri

--- my_functions/sample.js
+++ my_functions/sample.js
@@ -9,7 +9,7 @@ const handler = async (event) => {
     try {
         const database = (await clientPromise).db(process.env.MONGODB_DATABASE);
         const collection = database.collection(process.env.MONGODB_COLLECTION);
-        const results = await collection.find({});
+        const results = await collection.find();
         return {
             statusCode: 200,
             body: JSON.stringify(results),
