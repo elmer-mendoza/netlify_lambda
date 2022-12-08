4f16be7c8dae757a896644fc0151c7d976dfc35e
mdel <mdelefm@gmail.com>
2022-12-08 00:50:26 -0800

    console.log result

--- components/header/header.js
+++ components/header/header.js
@@ -5,6 +5,7 @@ function Header() {
 
 const handleSample = async() => {
    const result = await fetch('https://lustrous-crepe-401a2c.netlify.app/.netlify/functions/sample')
+   console.log(result)
 }
 
   return (
