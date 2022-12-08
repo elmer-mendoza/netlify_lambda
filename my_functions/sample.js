cfcb4565059537ddd262c376a69c45b936258114
mdel <mdelefm@gmail.com>
2022-12-08 03:26:34 -0800

    change uri

--- my_functions/sample.js
+++ my_functions/sample.js
@@ -4,12 +4,12 @@ require('dotenv').config()
 const mongoClient = new MongoClient('mongodb+srv://mdel_efm:Nov14151@mernshopping.jrbae.mongodb.net/?retryWrites=true&w=majority');
 // const mongoClient = new MongoClient(process.env.MONGODB_URI);
 
-// const clientPromise = mongoClient.connect();
+const clientPromise = mongoClient.connect();
 
 const handler = async (event) => {
     try {
-        const clientPromise =await mongoClient.connect();
-        console.log("Connected")
+       
+        
         // const collection = (await clientPromise).collection(process.env.MONGODB_COLLECTION);
         // const database = (await clientPromise).db(process.env.MONGODB_DATABASE);
         // const collection = database.collection(process.env.MONGODB_COLLECTION);
