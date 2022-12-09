969dc6b7069c18bbe1248b7d9aabedc4496e6f51
mdel <mdelefm@gmail.com>
2022-12-09 02:27:22 -0800

    revise mongodb connection

--- my_functions/getReviews.js
+++ my_functions/getReviews.js
@@ -8,10 +8,16 @@ let getData = (db, table, query) => {
 
 module.exports.handler = async event => {
   const dbConnection = await connectToDatabase(process.env.MONGODB_URI,process.env.MONGODB_DATABASE,{});
+  try{
   const data = await getData(dbConnection, 'reviews');
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (err) {
+    console.log(err); // output to netlify function log
+  } finally {
+    await dbConnection.close();
+  }
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(data),
-  };
 };
