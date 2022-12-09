d3a9f655ebe3a030dc43f1d94926a0ab041a8895
mdel <mdelefm@gmail.com>
2022-12-09 06:01:14 -0800

    refactor db connection

--- my_functions/getResumeData.js
+++ my_functions/getResumeData.js
@@ -15,7 +15,22 @@
 //   });
 // };
 
-const connectToMongoDB =require('../connectToMongoDB')
+// const connectToMongoDB =require('../connectToMongoDB')
+
+'use strict';
+const mongo = require('mongodb');
+const { MongoClient } = mongo;
+let db = null;
+
+let connectToMongoDB = (uri,dbName) => {
+  if (db && db.serverConfig.isConnected()) {
+    return Promise.resolve(db);
+  }
+  return MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }).then(client => {
+    db = client.db(dbName);
+    return db;
+  });
+};
 
 let getData = async( db,collection, query) => {
 
